640457331de954f2fb500e5da5607db571910135
Lukiam23 <matheus.1596@hot.com>
2017-07-04 14:08:55 -0300

    Update 12.py

--- Lista_7/12.py
+++ Lista_7/12.py
@@ -1,7 +1,7 @@
 #-*-coding:utf8;-*-
 
 unidade_milhar=["um","dois","três","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","catorze","quinze","dezesseis","dezessete","dezoito","dezenove"]
-centena=["cem","duzentos","trezentos","quatrocentos","quinhentos","seicentos","oitocentos","novecentos"]
+centena=["cem","duzentos","trezentos","quatrocentos","quinhentos","seicentos","setecentos","oitocentos","novecentos"]
 dezena=["dez","vinte","trinta","quarenta","cinquenta","secenta","setenta","oitenta","noventa"]
 string=["mil"]
 entrada=int(input("Insira a data:"))
@@ -14,18 +14,17 @@ else:
         string.insert(0,unidade_milhar[int(pedaco[0])-1])
     if pedaco[2]=="0" and pedaco[3]=="0":
         string.append(centena[int(pedaco[1])-1])
-    elif pedaco[1]=="1" and pedaco[2]=="0":
+    elif pedaco[1]=="1":
         string.append("cento e")
-        string.append(unidade_milhar[int(pedaco[3])-1])
-        
-    elif int(pedaco[2])>1:   
+    elif int(pedaco[1])>1 and pedaco[1]!="0":
         string.append(centena[int(pedaco[1])-1]+" e")
+    else:
+        string.append("e")
+    if pedaco[2]=="0":
         string.append(unidade_milhar[int(pedaco[3])-1])
-
-print((" ").join(string))
-        
-
-
-
-
-
+    elif pedaco[2]=="1":
+        string.append(unidade_milhar[int(pedaco[2]+pedaco[3])-1])
+    elif int(pedaco[2])>1:
+        string.append(dezena[int(pedaco[2])-1])
+        string.append(unidade_milhar[int(pedaco[3])-1])
+    print((" ").join(string))
