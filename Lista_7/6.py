897db9cc6013b18b99623d843a3421d5a49e829c
Lukiam23 <matheus.1596@hot.com>
2017-07-04 14:36:46 -0300

    Add files via upload

---
+++ Lista_7/6.py
@@ -0,0 +1,3 @@
+string=input("Insira a palavra: ")
+for i in range(0,len(string)+1):
+	print(string[0:i])
---
+++ Lista_7/7.py
@@ -0,0 +1,3 @@
+string=input("Insira a palavra: ")
+for i in range(1,len(string)+1):
+	print(string[-i:])
