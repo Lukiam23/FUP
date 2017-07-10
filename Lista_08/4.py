6cd290f7d397146bb2c6c627a540e73f793e5503
Lukiam23 <matheus.1596@hot.com>
2017-07-10 16:36:43 -0300

    Add files via upload

---
+++ Lista_08/4.py
@@ -0,0 +1,7 @@
+for i in range(15):
+    n_documento=int(input("Insira o número do documeto: "))
+    cod_cliente = input("Insira o código do cliente: ")
+    data_vencimento=input("Insira a data de vencimento: ")
+    data_pagamento=input("Insir a data de pagamento: ")
+    valor_conta=float(input("Insira o valor da conta: "))
+    valor_juros=float(input("Insira o valor dos juros: "))
